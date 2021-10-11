8fedb93e95b2449d09343f83452d18eba5bee9b6
xingsai <1035086033@qq.com>
2021-10-11 13:40:57 +0800

    1.地图标注修改

--- src/components/map.vue
+++ src/components/map.vue
@@ -6,6 +6,8 @@
 
 <script>
 import locationIcon from "@/assets/img/mark.png";
+import locationIcon1 from "@/assets/img/mark1.png";
+import locationIcon2 from "@/assets/img/mark2.png";
 
 import { getChestDetail } from "@/utils/api.js";
 
@@ -50,7 +52,12 @@ export default {
       var map = new BMap.Map("allmap"); // 创建地图实例
       map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
       map.clearOverlays();
-      var myIcon = new BMap.Icon(locationIcon, new BMap.Size(52, 50));
+      var myIcon1 = new BMap.Icon(locationIcon1, new BMap.Size(52, 50), {
+        imageSize: new BMap.Size(40, 44),
+      });
+      var myIcon2 = new BMap.Icon(locationIcon2, new BMap.Size(52, 50), {
+        imageSize: new BMap.Size(40, 44),
+      });
 
       this.devicesList.forEach((item, index) => {
         //自定义信息窗口内容
@@ -59,7 +66,7 @@ export default {
 
         var point = new BMap.Point(item.lng, item.lat); // 创建点坐标
         var marker = new BMap.Marker(point, {
-          icon: myIcon,
+          icon: item.status == 2 ? myIcon2 : myIcon1,
         });
         map.addOverlay(marker);
         // //点击图标时候调用信息窗口
@@ -71,28 +78,28 @@ export default {
             let goods = res.data.data.goods;
             console.log(goods);
             let srt = "";
-            for (var i = 0; i < goods.length; i++) {
-              srt =
-                srt +
-                '<p style="font-size:13px;margin: 5px 0;border:1px solid #999;padding:5px;box-sizing:border-box;">' +
-                "名称：" +
-                goods[i].name +
-                " &nbsp;&nbsp;&nbsp;<br/>品牌：" +
-                goods[i].brand +
-                " &nbsp;&nbsp;&nbsp;<br/>过期时间：" +
-                goods[i].expiration_at +
-                "</p>";
-            }
+            // for (var i = 0; i < goods.length; i++) {
+            //   srt =
+            //     srt +
+            //     '<p style="font-size:13px;margin: 5px 0;border:1px solid #999;padding:5px;box-sizing:border-box;">' +
+            //     "名称：" +
+            //     goods[i].name +
+            //     " &nbsp;&nbsp;&nbsp;<br/>品牌：" +
+            //     goods[i].brand +
+            //     " &nbsp;&nbsp;&nbsp;<br/>过期时间：" +
+            //     goods[i].expiration_at +
+            //     "</p>";
+            // }
             console.log(srt);
+            let color = item.status == 2 ? "#999" : "#999";
             var sContent =
-              '<div style="width:300px;"><p style="font-size:13px;margin: 5px 0;">地址：' +
+              '<div style="width:300px;color:' +
+              color +
+              '"><p style="font-size:13px;margin: 5px 0;">序号：' +
+              item.product +
+              '</p><p style="font-size:13px;margin: 5px 0;">地址：' +
               item.address +
-              '</p><p style="font-size:13px;margin: 5px 0;">坐标：' +
-              item.lng +
-              "," +
-              item.lat +
               "</p>" +
-              srt +
               "</div>";
             var infoWindow = new BMap.InfoWindow(sContent);
             marker.openInfoWindow(infoWindow);
